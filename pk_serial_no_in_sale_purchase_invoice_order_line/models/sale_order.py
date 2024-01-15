0505a0e3f0d42134c287d7754f4eea2461577de9
Irfan Ullah <155345293+pythonodoo@users.noreply.github.com>
2024-01-15 12:13:45 +0500

    add pk_serial_no_in_sale_purchase_invoice module

---
+++ pk_serial_no_in_sale_purchase_invoice_order_line/models/sale_order.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.onchange('order_line')
+    def _onchange_order_line_set_sn(self):
+        sl_no = 1
+        for line in self.order_line:
+            if line.display_type not in ['line_note', 'line_section']:
+                line.sl_no = sl_no
+                sl_no += 1
+            else:
+                line.sl_no = False
+
