05bb9c931438b1454423feadb7300b3efa44c6f6
pythonodoo <irfanteaches@gmail.com>
2024-01-16 15:26:07 +0500

    add image module

---
+++ pk_image_in_sale_account_move_line/__manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    "name": "Sale Order Line and Account Move Line Images",
+    "summary": "Images In Sale order line and Account move line",
+    "version": "15.0.0.0",
+    "category": 'Sales',
+    "website": "https://tech4sab@gmail.com",
+    "description": """Images In Sale order line and Account move line  """,
+    'author': 'PK Meta Code',
+    'company': 'PK Meta Code',
+    "depends": ['sale_management', 'account'],
+    "data": [
+        'views/sale_order_line_image.xml',
+        'views/invoice_order_line_image.xml',
+     ],
+    "auto_install": False,
+    "installable": True,
+
+}
