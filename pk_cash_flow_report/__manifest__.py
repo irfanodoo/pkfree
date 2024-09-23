714be2ab868d050d5e57d42fe89477cb11e85d9f
pythonodoo <irfanteaches@gmail.com>
2024-09-23 14:14:02 +0500

    [ADD] pk_cash_flow_report

---
+++ pk_cash_flow_report/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cash Flow Statement Report",
+
+    'summary': "Add Company Logo to Cash Flow Statement Report",
+    'author': "Irfan Ullah",
+    'website': "https://www.lahoreanalytica.com",
+    'category': 'Accounting',
+    'version': '17.0.0.0',
+    'license': 'LGPL-3',
+
+     'depends': ['base', 'account_accountant'],
+
+     'data': [
+        'report/cash_flow_report.xml',
+    ],
+}
+
